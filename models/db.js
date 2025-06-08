dcffeba9f18fb7a81c3095efa1fcf4d724df219b
Bilsyp <86169804+Bilsyp@users.noreply.github.com>
2025-06-08 20:14:27 +0700

    Use env vars for DB config

--- models/db.js
+++ models/db.js
@@ -4,11 +4,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "spkuser",
-  password: "spkpass",
-  database: "spk_electre",
-  port: 3306,
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "spkuser",
+  password: process.env.DB_PASS || "spkpass",
+  database: process.env.DB_NAME || "spk_electre",
+  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
